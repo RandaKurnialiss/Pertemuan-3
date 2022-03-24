cf0fbdd8e6c26dcd0e7e25c34d11fc5c741808a6
Randa Kurnialis <randakurnialis@gmail.com>
2022-03-24 12:12:32 +0700

    Polymorphism B

---
+++ src/main/kotlin/id/ac/polbeng/randa/test_kelas/20_PolymorphismB.kt
@@ -0,0 +1,26 @@
+package id.ac.polbeng.randa.test_kelas
+
+open class MyBase {
+
+
+    open fun think () {
+
+        println("Hey!! i am thinking ")
+    }
+}
+class MyDerived: MyBase() {
+
+    override fun think() {
+
+        println("I Am from Child")
+    }
+
+}
+
+fun main() {
+    val myBase = MyBase()
+    myBase.think()
+    var myDerived = MyDerived()
+    myDerived.think()
+
+}
