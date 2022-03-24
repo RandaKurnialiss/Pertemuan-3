e4b52b1e59e3b92f719f183541c8b4f0f4fc94fe
Randa Kurnialis <randakurnialis@gmail.com>
2022-03-24 12:24:02 +0700

    Ketika sebuah kelas dideklarasikan didalam kelas lain, maka ia disebut dengan nested class. Nested class di Kotlin sama dengan static nested class di Java. Hal yang perlu diperhatikan dalam nested class : 1. Sebuah nested class tidak dapat mengakses anggota dari kelas luarnya. 2. Untuk mengakses anggota dari nested class, kita gunakan („.‟) operator dengan outer kelasnya.

---
+++ src/main/kotlin/id/ac/polbeng/randa/test_kelas/28_NestedInnerClass.kt
@@ -0,0 +1,15 @@
+package id.ac.polbeng.randa.test_kelas
+
+class Outer {
+    val a = "Outside Nested class."
+    class Nested {
+        val b = "Inside Nested class."
+        fun callMe() = "Function call from inside Nested class."
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Outer.Nested().b)
+    val nested = Outer.Nested()
+    println(nested.callMe())
+}
